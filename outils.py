11726e9b04b77283fa75293f018d65d1b33d2007
eric.herbert <eric.herbert@univ-paris-diderot.fr>
2021-03-31 10:46:02 +0200

    update correlation covariance

---
+++ outils.py
@@ -0,0 +1,17 @@
+
+
+import numpy as np
+
+def covariance(x,y):
+    mean_x = np.mean(x)
+    mean_y = np.mean(y)
+    cov = np.mean( (x-mean_x) * (y-mean_y) )
+    return cov
+    
+    
+def correlation(x,y):
+    cov = covariance(x,y)
+    std_x = np.std(x)
+    std_y = np.std(y)
+    r = cov / std_x / std_y
+    return r
